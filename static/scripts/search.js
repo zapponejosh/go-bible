0de8e767f557911e8ddf00a17b939ac6dcde5d06
Josh Zappone <zappone.josh@gmail.com>
2022-03-08 20:54:04 -0800

    keep the persist the search input value

---
+++ static/scripts/search.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  let params = new URL(document.location).searchParams;
+  let search = params.get("search");
+  let references = document.querySelectorAll(".reference");
+  references.forEach((refNode) => {
+    refNode.href = refNode.href + "&search=" + search;
+  });
+  let searchInput = document.getElementById("search");
+  searchInput.value = search;
+});
