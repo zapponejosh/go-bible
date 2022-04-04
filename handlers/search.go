b5e8f1cc4884cd22db40ee2a1e2eb8962081fb9e
Josh Zappone <zappone.josh@gmail.com>
2022-04-03 20:45:42 -0700

    generating query from params with filters

--- handlers/search.go
+++ handlers/search.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -32,6 +33,50 @@ type TemplateData struct {
 	Count   int
 }
 
+type Parser func(string) (interface{}, error)
+
+type param struct {
+	Type   string
+	Column string // for filters only
+	Parser Parser
+}
+
+func IntParser(v string) (interface{}, error) {
+	return strconv.Atoi(v)
+}
+
+func NoopParser(v string) (interface{}, error) {
+	return v, nil
+}
+func SearchParser(v string) (interface{}, error) {
+	terms := strings.Split(v, " ")
+	phrase := false
+	t1 := strings.Index(terms[0], "\"")
+	last := terms[len(terms)-1]
+	tfinal := strings.Index(last, "\"")
+	if t1 == 0 && tfinal == len(last)-1 {
+		phrase = true
+	}
+
+	var queryTerms string
+	if phrase {
+		queryTerms = strings.Join(terms, "<->")
+	} else {
+		queryTerms = strings.Join(terms, "&")
+	}
+	return queryTerms, nil
+}
+
+func PaginationParser(v string) (interface{}, error) {
+	offset, err := strconv.Atoi(v)
+	if err != nil || offset <= 0 {
+		offset = 0
+	} else {
+		offset = (offset - 1) * 50
+	}
+	return offset, nil
+}
+
 func NewIndexHandler(db *sql.DB, indexTemp *template.Template) *indexHandler {
 	return &indexHandler{db: db, indexTemp: indexTemp}
 }
@@ -42,29 +87,14 @@ func (h indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	q := r.URL.Query()
-	// format query in slice
-	terms := strings.Split(strings.Join(q["search"], " "), " ")
-	offset, err := strconv.Atoi(strings.Join(q["p"], ""))
-	bookFilter := strings.Join(q["bookFilter"], " ")
-	testamentFilter := strings.Join(q["testamentFilter"], " ")
-	sectionFilter := strings.Join(q["sectionFilter"], " ")
-	fmt.Println(bookFilter, testamentFilter, sectionFilter)
-	if err != nil || offset <= 0 {
-		offset = 0
-	} else {
-		offset = (offset - 1) * 50
-	}
-	// allow for phrases
-	phrase := false
-	t1 := strings.Index(terms[0], "\"")
-	last := terms[len(terms)-1]
-	tfinal := strings.Index(last, "\"")
-	if t1 == 0 && tfinal == len(last)-1 {
-		phrase = true
+	query, args, err := genQuery(r.URL.Query())
+	if err != nil {
+		log.Println("error parsing params or generating query", err)
+		http.Error(w, "search error", http.StatusInternalServerError)
+		return
 	}
 
-	ftsQuery, results, resultCount, err := searchBible(terms, h.db, phrase, offset)
+	ftsQuery, results, resultCount, phrase, err := searchBible(query, args, h.db)
 	if err != nil {
 		log.Println("error searching terms", err)
 		http.Error(w, "search error", http.StatusInternalServerError)
@@ -123,15 +153,84 @@ func highlightTerm(fts []string, results []*VerseResult, isPhrase bool) {
 
 }
 
-func searchBible(terms []string, DB *sql.DB, isPhrase bool, offset int) ([]string, []*VerseResult, int, error) {
-	var queryTerms string
-	if isPhrase {
-		queryTerms = strings.Join(terms, "<->")
-	} else {
-		queryTerms = strings.Join(terms, "&")
+func genQuery(values url.Values) (string, []interface{}, error) {
+
+	var (
+		validParams = map[string]param{
+			"search": {
+				Type:   "search",
+				Parser: SearchParser,
+			},
+			"p": {
+				Type:   "offset",
+				Parser: PaginationParser,
+			},
+			"testamentFilter": {
+				Type:   "filter",
+				Column: "k.testament",
+			},
+			"sectionFilter": {
+				Type:   "filter",
+				Column: "k.section",
+			},
+			"bookFilter": {
+				Type:   "filter",
+				Column: "k.short_name",
+			},
+		}
+		searchArg string
+		offsetArg string
+		filters   []string
+		args      []interface{}
+		count     = 1
+	)
+
+	for key := range values {
+		f, found := validParams[key]
+		if !found {
+			// fail silently and skip to next param
+			continue
+		}
+		// leave out params with no values
+		if str := strings.TrimSpace(values.Get(key)); str == "" {
+			continue
+		}
+
+		parser := f.Parser
+		if parser == nil {
+			// if a parser wasn't supplied, use the noop
+			parser = NoopParser
+		}
+
+		val, err := parser(values.Get(key))
+		if err != nil {
+			return "", nil, fmt.Errorf("query generator: error parsing key %s: %w", key, err)
+		}
+
+		args = append(args, val)
+		switch f.Type {
+		case "filter":
+			col := f.Column
+			filters = append(filters, fmt.Sprintf("%s = $%d", col, count))
+		case "search":
+			searchArg = fmt.Sprintf("$%d", count)
+		case "offset":
+			offsetArg = fmt.Sprintf("$%d", count)
+		}
+		count++
 	}
 
-	rows, err := DB.Query(`SELECT b.content,
+	// no pagination param provided set default to zero
+	if offsetArg == "" {
+		offsetArg = fmt.Sprintf("$%d", count)
+		args = append(args, 0)
+	}
+
+	where := strings.Join(filters, " AND ")
+	if where != "" {
+		where = "WHERE " + where
+	}
+	query := fmt.Sprintf(`SELECT b.content,
   b.book,
   b.chapter,
   b.verse,
@@ -144,14 +243,25 @@ FROM (
       verse,
       query
     FROM bible,
-      to_tsquery('english', $1) query
+      to_tsquery('english', %s) query
     WHERE query @@ fts
   ) as b
   LEFT JOIN books k on k.short_name = b.book
-ORDER BY k.book_num LIMIT 50 OFFSET $2;`, queryTerms, offset)
+	%s
+ORDER BY k.book_num LIMIT 50 OFFSET %s;`, searchArg, where, offsetArg)
+	return query, args, nil
+}
+
+func searchBible(search string, args []interface{}, DB *sql.DB) ([]string, []*VerseResult, int, bool, error) {
+	isPhrase := false
+	if strings.Contains(search, "<->") {
+		isPhrase = true
+	}
+
+	rows, err := DB.Query(search, args...)
 	if err != nil {
 		// handle this error
-		return nil, nil, 0, err
+		return nil, nil, 0, isPhrase, err
 	}
 	defer rows.Close()
 
@@ -168,12 +278,12 @@ ORDER BY k.book_num LIMIT 50 OFFSET $2;`, queryTerms, offset)
 		err = rows.Scan(&content, &result.Book, &result.Chapter, &result.Verse, &query, &resultCount)
 		if err != nil {
 			// handle this error
-			return nil, nil, 0, err
+			return nil, nil, 0, isPhrase, err
 		}
 
 		err = xml.Unmarshal([]byte(content), &result.Content)
 		if err != nil {
-			return nil, nil, 0, err
+			return nil, nil, 0, isPhrase, err
 		}
 
 		if isPhrase {
@@ -191,9 +301,9 @@ ORDER BY k.book_num LIMIT 50 OFFSET $2;`, queryTerms, offset)
 	// get any error encountered during iteration
 	err = rows.Err()
 	if err != nil {
-		return nil, nil, 0, err
+		return nil, nil, 0, isPhrase, err
 	}
 
-	return ftsQuery, results, resultCount, err
+	return ftsQuery, results, resultCount, isPhrase, err
 
 }
